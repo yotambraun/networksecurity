8c75ad63d0c80110a053ce976212b94b7284863e
yotam <yotambarun93@gmail.com>
2024-12-28 18:21:48 +0200

    complete datat validation

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 import sys
 
@@ -13,7 +14,14 @@ if __name__ == "__main__":
         data_ingestion = DataIngestion(dataingestconfig)
         logging.info("initiate the data ingestion process")
         dataingestionartifact = data_ingestion.initiate_data_ingestion()
+        logging.info("Data ingestion process completed successfully")
         print(dataingestionartifact)
+        data_validation_config = DataValidationConfig(trainingpipelineconfig)
+        data_validation = DataValidation(dataingestionartifact, data_validation_config)
+        logging.info("initiate the data validation process")
+        data_validation_artifact = data_validation.initiate_data_validation()
+        logging.info("Data validation process completed successfully")
+        print(data_validation_artifact)
        
     except Exception as e:
         raise NetworkSecurityException(e,sys)
--- networksecurity/components/data_validation.py
+++ networksecurity/components/data_validation.py
@@ -2,7 +2,7 @@ from networksecurity.entity.artifact_entity import DataIngestionArtifact,DataVal
 from networksecurity.entity.config_entity import DataValidationConfig
 from networksecurity.exception.exception import NetworkSecurityException 
 from networksecurity.logging.logger import logging 
-from networksecurity.constant.training_pipeline import SCHEMA_FILE_PATH
+from networksecurity.constants.training_pipeline import SCHEMA_FILE_PATH
 from scipy.stats import ks_2samp
 import pandas as pd
 import os,sys
